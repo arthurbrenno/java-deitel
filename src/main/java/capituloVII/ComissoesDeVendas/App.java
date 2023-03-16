59f23650f602d2f49164cd24981297a621585b22
Arthur-Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-03-16 17:47:46 -0300

    att calculo de comissoes

--- src/main/java/capituloVII/ComissoesDeVendas/App.java
+++ src/main/java/capituloVII/ComissoesDeVendas/App.java
@@ -1,14 +1,33 @@
 package capituloVII.ComissoesDeVendas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class App {
     
-    private static double calcularComissao(double startInterval, double endInterval) {
-        return 2.0;
+    private static ArrayList<Employee> employees = new ArrayList<>();
+    private static final Scanner scanner = new Scanner(System.in);
+    
+    private static boolean estaCheio(int[] intervalo) {
+        for (int element : intervalo) {
+            if (element == 0) {return false;}
+        }
+        return true;
     }
     
     public static void main(String[] args) {
-        System.out.println("Hi");
+        int[] intervalos = new int[11];
+        
+        int i = 1;
+        //substituir por do-while depois
+        while (!estaCheio(intervalos)) {
+            System.out.printf("Salario %d: ", i++);
+            int salario = (int) scanner.nextDouble();
+            if (salario == -1) { break; }
+            intervalos[salario / 100]++;
+        }
+        System.out.println(Arrays.toString(intervalos));
     }
-
 
 }
--- src/main/java/capituloVII/ComissoesDeVendas/Employee.java
+++ src/main/java/capituloVII/ComissoesDeVendas/Employee.java
@@ -1,5 +1,21 @@
 package capituloVII.ComissoesDeVendas;
 
 public class Employee {
+    private static final double SALARIO_SEMANAL = 200;
+    private double vendasBrutas;
+    private static double PERCENTUAL = 9;
+    private final double salarioFinal = SALARIO_SEMANAL + (vendasBrutas * (PERCENTUAL / 100));
+
+    public Employee(double vendasBrutas) {
+        this.vendasBrutas = vendasBrutas;
+    }
+
+    public void setPercentual(double PERCENTUAL) {
+        Employee.PERCENTUAL = PERCENTUAL;
+    }
+
+    public double getSalarioFinal() {
+        return this.salarioFinal;
+    }
     
 }
