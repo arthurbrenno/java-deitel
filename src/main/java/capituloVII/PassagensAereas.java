b665cb6d9b4f24c854690c0363d7ee4caa956909
Arthur-Brenno <64020210+arthurbrenno@users.noreply.github.com>
2023-03-27 10:41:05 -0300

    app de passagens aereas

--- src/main/java/capituloVII/PassagensAereas.java
+++ src/main/java/capituloVII/PassagensAereas.java
@@ -1,38 +1,98 @@
 package capituloVII;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class PassagensAereas {
     private static final int CAPACIDADE_DE_ASSENTOS = 10;
     private static boolean[] assentos = new boolean[CAPACIDADE_DE_ASSENTOS + 1];
+    private static final Random random = new Random();
+    private static final Scanner scanner = new Scanner(System.in);
+    private static int classeEscolhida;
 
     public static void main(String[] args) {
         
         for (int i = 0; i < assentos.length; i++) {
             assentos[i] = false;
         }
-        System.out.println(Arrays.toString(assentos));
-        displayAssentos(2);
+
+
+        System.out.println("Please type 1 for First Class");
+        System.out.println("Please type 2 for Economy");
+        System.out.print(">> ");
+        classeEscolhida = scanner.nextInt();
+        if (!todasPoltronasDaClasseCheias()) {
+            preencherAssento();
+            System.out.println(Arrays.toString(assentos));
+            displayAssentos();
+        }
+        else {
+            System.out.println("As poltronas dessa classe estão cheias, deseja mudar para outra?");
+            System.out.print(">> ");
+        }
 
     }
 
 
-    private static void displayAssentos(int classe) {
+    private static void displayAssentos() {
         System.out.println("Assentos:");
         for (int i = 0; i < assentos.length / 2; i++) {
             System.out.printf("   [%d]  ", i+1);
         }
         System.out.println();
-        switch (classe) {
+        switch (classeEscolhida) {
             case 1: 
-                for (int i = 1; i < assentos.length / 2; i++) {
+                for (int i = 1; i <= assentos.length / 2; i++) {
                     System.out.print("  " + assentos[i] + " ");
                 } break;
             case 2:
-                for (int i = assentos.length / 2; i < assentos.length - 1; i++) {
+                for (int i = (CAPACIDADE_DE_ASSENTOS / 2) + 1; i < assentos.length; i++) {
                     System.out.print("  " + assentos[i] + " ");
                 } break;
             default: System.out.println("Escreva uma classe válida!");
         }
+        System.out.println();
+    }
+
+    private static void preencherAssento() {
+        assentos[getAssentoVazio()] = true;
+    }
+
+    private static int getAssentoVazio() {
+        int indexVazio = 0;
+        switch (classeEscolhida) {
+            case 1: 
+                for (int i = assentos.length / 2; i >= 0; i--) {
+                    if (assentos[i] == false) {
+                        indexVazio = i; break;
+                    }
+                } break;
+            case 2:
+                for (int i = assentos.length - 1; i > assentos.length / 2; i--) {
+                    if (assentos[i] == false) {
+                        indexVazio = i; break;
+                    }
+                } break;
+        }
+        return indexVazio;
+    }
+
+    private static boolean todasPoltronasDaClasseCheias() {
+        switch (classeEscolhida) {
+            case 1: 
+                for (int i = 1; i <= assentos.length / 2; i++) {
+                    if (assentos[i] == false) {
+                        return false;
+                    }
+                } break;
+            case 2: 
+                for (int i = (CAPACIDADE_DE_ASSENTOS / 2) + 1; i < assentos.length; i++) {
+                    if (assentos[i] == false) {
+                        return false;
+                    }
+                }
+        }
+        return true;
     }
 }
